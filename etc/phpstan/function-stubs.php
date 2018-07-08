3500461d29dcd95b28e9bbce4a873c87546ba616
Peter Walther <pewa-github@rosasurfer.com>
2018-07-08 11:03:21 +0300

    add PHPStan configuration

---
+++ etc/phpstan/function-stubs.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+
+namespace rosasurfer\bfx\bin\logwatch {
+
+    /**
+     * @param  string $message [optional]
+     */
+    function help($message = null) {}
+
+    /**
+     * @param  string $entry
+     */
+    function processEntry($entry) {}
+}
