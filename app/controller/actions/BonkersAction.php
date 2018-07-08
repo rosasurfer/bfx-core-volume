bf9ffef64b2144dd0480f78e119711d4794fe727
Peter Walther <pewa-github@rosasurfer.com>
2018-07-08 22:02:02 +0300

    read licenses form the application configuration

--- app/controller/actions/BonkersAction.php
+++ app/controller/actions/BonkersAction.php
@@ -6,6 +6,7 @@ use rosasurfer\ministruts\Action;
 use rosasurfer\ministruts\ActionForward;
 use rosasurfer\ministruts\Request;
 use rosasurfer\ministruts\Response;
+use rosasurfer\config\Config;
 
 
 /**
@@ -24,14 +25,21 @@ class BonkersAction extends Action {
             $url = 'https'.strRight($url, -4);
             return new ActionForward('generic', $url, $redirect=true);
         }
+        $config = Config::getDefault();
 
-        // GET http://www.bankersfx.com/Paypal/TFV/index.php?id=TSR&lic={encoded-license}&rn=9&mt4={account}&ec=1
-        // {account}|{license}|A|{expiration}|mt4tfv|ok
+        // request:  GET https://www.bankersfx.com/Paypal/TFV/index.php?id=TSR&lic={encoded-license}&rn=9&mt4={account}&ec=1
+        // response: {account}|{license}|A|{expiration}|mt4tfv|ok
+        // response: ERROR: Membership Inactive|no
 
         $account = $request->getParameter('mt4') ?: '0';
-        $license = '{license}';
-        $expires = '{expiration}';
-        $reply   = $account.'|'.$license.'|A|'.$expires.'|mt4tfv|ok';
+        $license = $config->get('bfx.accounts.'.$account, false);
+        if ($license) {
+            $expires = date('Ymd', time() + 1*MONTH);
+            $reply   = $account.'|'.$license.'|A|'.$expires.'|mt4tfv|ok';
+        }
+        else {
+            $reply = 'ERROR: Unknown account. Please go to http://bfx.rosasurfer.com/ to configure access for your account.|no';
+        }
 
         Logger::log('reply: '.$reply, L_INFO);
         echo $reply;
