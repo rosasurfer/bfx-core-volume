03b06c6f059fdf838c0927af97de41199c74e8b6
Peter Walther <pewa-github@rosasurfer.com>
2025-04-06 08:26:10 +0300

    update application configuration

--- app/init.php
+++ app/init.php
@@ -14,12 +14,9 @@ require(($appRoot=dirname(__DIR__)).'/vendor/autoload.php');
 // php.ini settings
 error_reporting(E_ALL & ~E_DEPRECATED);
 
-PHP::ini_set('display_errors',     (string)(int)CLI             );
-PHP::ini_set('html_errors',        '0'                          );
-PHP::ini_set('error_log',          $appRoot.'/log/php-error.log');
-PHP::ini_set('log_errors',         '1'                          );
-PHP::ini_set('log_errors_max_len', '0'                          );
-PHP::ini_set('default_charset',    'UTF-8'                      );
+PHP::ini_set('log_errors',      '1'                          );
+PHP::ini_set('error_log',       $appRoot.'/log/php-error.log');
+PHP::ini_set('default_charset', 'UTF-8'                      );
 
 
 // create a new application
